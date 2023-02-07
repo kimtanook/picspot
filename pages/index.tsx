1f7f5bf2f4b27fdb8b3ad22d9c80172d7e682d15
frankkim92 <syncstar9@gmail.com>
2023-02-07 15:23:10 +0900

    Feat: add kakao map api - kakao map api, - .env setting by @frankkim92

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,21 @@
+import { Map } from 'react-kakao-maps-sdk';
+
 export default function Main() {
-  return <>Main</>;
+  return (
+    <>
+      <Map // 지도를 표시할 Container
+        center={{
+          // 지도의 중심좌표
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
+        style={{
+          // 지도의 크기
+          width: '100%',
+          height: '450px',
+        }}
+        level={6} // 지도의 확대 레벨
+      />
+    </>
+  );
 }
