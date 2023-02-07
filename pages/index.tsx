47ddf170ea03791d83841c419a3b1ace9053751f
Jaeyoung <dlawodud96@naver.com>
2023-02-07 14:36:50 +0900

    Add   main test for post CRUD 게시물CRUD구현을 위한 Main 수정 #5 @Imjaae

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,84 @@
+import Image from 'next/image';
+import styled from 'styled-components';
+
 export default function Main() {
-  return <>Main</>;
+  return (
+    <>
+      <input />
+      <div style={{ display: 'flex', gap: '10px', padding: '10px' }}>
+        <Categorys>지역</Categorys>
+        <Categorys>작가</Categorys>
+        <Categorys>지역</Categorys>
+        <Categorys>팔로우</Categorys>
+        <Categorys>게시물 작성</Categorys>
+      </div>
+      <div></div>
+      <div>
+        <ImageBox>
+          <Image
+            // src = {https://i.pinimg.com/564x/c7/e6/ba/c7e6bad3167a4d0188a4e5914e0dcb28.jpg}
+            src="/dog.jpeg"
+            alt="dog"
+            width={200}
+            height={200}
+            style={{
+              margin: '20px',
+            }}
+          />
+          <Image
+            // src = {https://i.pinimg.com/564x/c7/e6/ba/c7e6bad3167a4d0188a4e5914e0dcb28.jpg}
+            src="/dog.jpeg"
+            alt="dog"
+            width={200}
+            height={200}
+            style={{
+              margin: '20px',
+            }}
+          />
+          <Image
+            // src = {https://i.pinimg.com/564x/c7/e6/ba/c7e6bad3167a4d0188a4e5914e0dcb28.jpg}
+            src="/dog.jpeg"
+            alt="dog"
+            width={200}
+            height={200}
+            style={{
+              margin: '20px',
+            }}
+          />
+          <Image
+            // src = {https://i.pinimg.com/564x/c7/e6/ba/c7e6bad3167a4d0188a4e5914e0dcb28.jpg}
+            src="/dog.jpeg"
+            alt="dog"
+            width={200}
+            height={200}
+            style={{
+              margin: '20px',
+            }}
+          />
+          <Image
+            // src = {https://i.pinimg.com/564x/c7/e6/ba/c7e6bad3167a4d0188a4e5914e0dcb28.jpg}
+            src="/dog.jpeg"
+            alt="dog"
+            width={200}
+            height={200}
+            style={{
+              margin: '20px',
+            }}
+          />
+        </ImageBox>
+      </div>
+    </>
+  );
 }
+
+const Categorys = styled.button`
+  background-color: tomato;
+  width: 100px;
+  height: 40px;
+`;
+
+const ImageBox = styled.div`
+  border: tomato 1px solid;
+  display: flex;
+  flex-direction: row;
+`;
