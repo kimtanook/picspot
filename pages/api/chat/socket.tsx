d3f50ef7ee05f93c6af76cb2213477f2d28a48b0
kimtanook <seob6402@gmail.com>
2023-02-08 10:20:42 +0900

    Feat create chat service create chat server (socket io) by @kimtanook  #18

---
+++ pages/api/chat/socket.tsx
@@ -0,0 +1,37 @@
+import { NextApiRequest } from 'next';
+import { NextApiResponseServerIO } from 'types/chat';
+import { Server as ServerIO } from 'socket.io';
+import { Server as HttpServer } from 'http';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const socket = async (req: NextApiRequest, res: NextApiResponseServerIO) => {
+  console.log('New Socket.io server...✅');
+
+  const httpServer: HttpServer = res.socket.server as any;
+  const io = new ServerIO(httpServer, {
+    path: '/api/chat/socket',
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('enter_room', (room, user, toggleHandler) => {
+      socket.join(room);
+      toggleHandler();
+      socket.to(room).emit('enter', user);
+      socket.on('disconnecting', () => {
+        socket.to(room).emit('exit', user);
+      });
+    });
+    socket.on('message', (message) => {
+      socket.to(message.room).emit('message', message);
+    });
+    console.log(socket);
+  });
+  res.socket.server.io = io;
+};
+
+export default socket;
