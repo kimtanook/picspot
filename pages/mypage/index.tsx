2f54012beb5e6ebb1c13dd1d1783d302a7252ee5
Kidongg <dlrlehd7@daum.net>
2023-02-11 17:11:24 +0900

    Feat apply react-query to read, update, delete post
    - useQuery를 사용해 post 데이터를 조회 할 수 있도록 했습니다.
    - useMutation을 사용해 post 데이터를 수정, 삭제할 수 있도록 했습니다.
    - 이미지는 업로드 이후 수정할 수 있습니다.
    
    by @kidongg #28

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,3 +1,4 @@
+import { getDatas, deleteData, updataData } from '@/api';
 import Seo from '@/components/Seo';
 import { dbService, storageService } from '@/firebase';
 import {
@@ -10,93 +11,103 @@ import {
 } from 'firebase/firestore';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 
+//! 이미지 수정 구현하기
 export default function Mypage() {
-  const [arr, setArr] = useState([]);
+  const queryClient = useQueryClient();
 
   const [editTitle, setEditTitle] = useState('');
-  const [editImageUpload, setEditImageUpload]: any = useState(null);
-  const [editImageUrl, setEditImageUrl] = useState('');
+  const [editImgUpload, setEditImgUpload]: any = useState(null);
+  const [editImgUrl, setEditImgUrl] = useState('');
 
-  const editState = {
-    test: editTitle,
-    url: editImageUrl,
+  const editState: any = {
+    title: editTitle,
+    url: editImgUrl,
   };
 
-  //* 스토어에서 데이터 가져오기
-  const getTestData = () => {
-    const q = query(collection(dbService, 'test'));
+  //* useQuery 사용해서 데이터 불러오기
+  const { data, isLoading, isError } = useQuery('datas', getDatas);
+  console.log('data: ', data);
 
-    onSnapshot(q, (snapshot) => {
-      const newArr: any = [];
-      snapshot.docs.map((doc) => {
-        const newObj = {
-          id: doc.id,
-          ...doc.data(), // doc.data() : { text, createdAt, ...  }
-        };
-        newArr.push(newObj);
-        setArr(newArr);
-      });
-      // console.log('newArr: ', newArr);
+  //* useMutation 사용해서 데이터 삭제하기
+  const { mutate: onDeleteData } = useMutation(deleteData);
+
+  //* 삭제버튼 눌렀을때 실행하는 함수
+  const onClickDeleteData = (docId: any) => {
+    console.log('삭제버튼을 클릭했습니다.');
+    onDeleteData(docId, {
+      onSuccess: () => {
+        console.log('삭제 요청 성공');
+        queryClient.invalidateQueries('datas');
+      },
+      onError: () => {
+        console.log('삭제 요청 실패');
+      },
     });
   };
 
-  //* 스토어 데이터 출력하기
-  useEffect(() => {
-    getTestData();
-  }, []);
+  //* useMutation 사용해서 데이터 수정하기
+  const { mutate: onUpdataData } = useMutation(updataData);
 
-  //* 스토어에 데이터 수정하기
-  const editData = (docId: any) => {
+  //* 수정버튼 눌렀을때 실행하는 함수
+  const onClickUpdateData = (data: any) => {
     console.log('수정버튼을 클릭했습니다.');
-    updateDoc(doc(dbService, 'test', docId), editState);
-  };
-
-  //* 스토어에 데이터 삭제하기
-  const deleteData = (docId: any) => {
-    console.log('삭제버튼을 클릭했습니다.');
-    deleteDoc(doc(dbService, 'test', docId));
+    onUpdataData(data, {
+      onSuccess: () => {
+        console.log('수정 요청 성공');
+        queryClient.invalidateQueries('datas');
+      },
+      onError: () => {
+        console.log('수정 요청 실패');
+      },
+    });
   };
 
   //* 스토리지에 이미지 업로드하기
   //* 스토리지에 있는 이미지 스냅샷해서 URL 가져오기
-  const editUpload = () => {
-    if (editImageUpload === null) return;
+  const onClickEditImgUpload = () => {
+    if (editImgUpload === null) return;
 
-    // console.log('editImageUpload: ', editImageUpload);
-    const imageRef = ref(storageService, `images/${editImageUpload.name}`);
-    uploadBytes(imageRef, editImageUpload).then((snapshot) => {
+    const imageRef = ref(storageService, `images/${editImgUpload.name}`);
+    uploadBytes(imageRef, editImgUpload).then((snapshot) => {
       getDownloadURL(snapshot.ref).then((url) => {
         console.log('사진이 업로드 되었습니다.');
         console.log('url: ', url);
-        setEditImageUrl(url);
+        setEditImgUrl(url);
       });
     });
   };
+
+  if (isLoading) return <h1>로딩 중입니다.</h1>;
+  if (isError) return <h1>연결이 원활하지 않습니다.</h1>;
 
   return (
     <div>
       <Seo title="My" />
       <h1>마이페이지임</h1>
-      {arr.map((item: any) => (
+      {data.map((item: any) => (
         <div key={item.id}>
-          <h3>{item.test}</h3>
-          <input
-            onChange={(e) => {
-              // console.log(e.target.value);
-              setEditTitle(e.target.value);
-            }}
-          />
-          <button onClick={() => editData(item.id)}>수정</button>
-          <button onClick={() => deleteData(item.id)}>삭제</button>
+          <h3>{item.title}</h3>
           <input
             type="file"
             onChange={(event: any) => {
-              setEditImageUpload(event.target.files[0]);
+              setEditImgUpload(event.target.files[0]);
             }}
           />
-          <button onClick={editUpload}>업로드</button>
+          <button onClick={onClickEditImgUpload}>업로드</button>
+          <input
+            onChange={(e) => {
+              setEditTitle(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => onClickUpdateData({ id: item.id, ...editState })}
+          >
+            수정
+          </button>
+          <button onClick={() => onClickDeleteData(item.id)}>삭제</button>
           <Image src={item.url} alt="image" height={100} width={100} />
         </div>
       ))}
