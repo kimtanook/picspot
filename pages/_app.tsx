30b5989170c0f30d33df7854bde3aeb4ee8e6781
Kidongg <dlrlehd7@daum.net>
2023-02-11 17:04:28 +0900

    Feat react-query
    - QueryClientProvider를 전역에 적용했습니다.
    
    by @kidongg #28

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={client}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
