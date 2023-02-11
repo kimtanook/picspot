1f2b0f36ad0d135d438fb8c6e3626a5710555d5f
Kidongg <dlrlehd7@daum.net>
2023-02-11 17:10:21 +0900

    Feat apply react-query to create post
    - useMutation을 사용해 post 데이터를 추가할 수 있도록 했습니다.
    - 이미지는 업로드 이후 추가할 수 있습니다.
    
    by @kidongg #28

--- components/main/PostForm.tsx
+++ components/main/PostForm.tsx
@@ -2,56 +2,68 @@ import { dbService, storageService } from '@/firebase';
 import { addDoc, collection } from 'firebase/firestore';
 import React, { useState } from 'react';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { useMutation, useQueryClient } from 'react-query';
+import { addData } from '@/api';
 
 const PostForm = () => {
-  const [test, setTest] = useState('');
+  const [title, setTitle] = useState('');
   const [imageUpload, setImageUpload]: any = useState(null);
-  const [imageUrl, setImageUrl] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
 
-  const initsialState = {
-    test: test,
-    url: imageUrl,
+  const postState: any = {
+    title: title,
+    url: imgUrl,
+  };
+
+  //* useMutation 사용해서 데이터 추가하기
+  const { mutate: onAddData } = useMutation(addData);
+
+  //* 추가버튼 눌렀을때 실행하는 함수
+  const onClickAddData = async () => {
+    onAddData(postState, {
+      onSuccess: () => {
+        console.log('추가 요청 성공');
+      },
+      onError: () => {
+        console.log('추가 요청 실패');
+      },
+    });
   };
 
   //* 스토리지에 이미지 업로드하기
-  //* 스토리지에 있는 이미지 스냅샷해서 URL 가져오기
-  const upload = () => {
-    if (imageUpload === null) return;
+  //* 스토리지에 있는 이미지 URL 가져오기
+  const onClickImgUpload = () => {
+    if (imageUpload === null) {
+      alert('이미지를 추가해주세요.');
+      return;
+    }
 
-    // console.log('imageUpload: ', imageUpload);
     const imageRef = ref(storageService, `images/${imageUpload.name}`);
     uploadBytes(imageRef, imageUpload).then((snapshot) => {
       getDownloadURL(snapshot.ref).then((url) => {
         console.log('사진이 업로드 되었습니다.');
         console.log('url: ', url);
-        setImageUrl(url);
+        setImgUrl(url);
       });
     });
   };
 
-  //* 스토어에 데이터 저장하기
-  const onClickTest = async () => {
-    await addDoc(collection(dbService, 'test'), initsialState);
-    console.log('데이터가 저장되었습니다.');
-  };
-
   return (
-    <div>
+    <>
       <input
         type="file"
         onChange={(event: any) => {
           setImageUpload(event.target.files[0]);
         }}
       />
-      <button onClick={upload}>업로드</button>
+      <button onClick={onClickImgUpload}>업로드</button>
       <input
         onChange={(e) => {
-          e.target.value;
-          setTest(e.target.value);
+          setTitle(e.target.value);
         }}
       />
-      <button onClick={onClickTest}>추가</button>
-    </div>
+      <button onClick={onClickAddData}>추가</button>
+    </>
   );
 };
 
