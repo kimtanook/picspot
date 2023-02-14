2cb9f3811355f1a8ca62b882c5d1ab2ce725600b
Kidongg <dlrlehd7@daum.net>
2023-02-14 19:35:31 +0900

    Feat apply query to get datas
    Use query, orderby function from firestore to query feature
    
    by @kidongg #41

--- api.tsx
+++ api.tsx
@@ -4,15 +4,19 @@ import {
   deleteDoc,
   doc,
   getDocs,
+  orderBy,
+  query,
   updateDoc,
 } from 'firebase/firestore';
 import { dbService } from './firebase';
 
 //* 스토어에서 데이터 불러오기
 export const getDatas = async () => {
+  const q = query(collection(dbService, 'test'), orderBy('createdAt', 'desc'));
+
   const response: any = [];
 
-  const querySnapshot = await getDocs(collection(dbService, 'postTest'));
+  const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
     response.push({ id: doc.id, ...doc.data() });
   });
@@ -24,20 +28,20 @@ export const getDatas = async () => {
 //* 스토어에 데이터 추가하기
 export const addData: any = (data: any) => {
   console.log('data: ', data);
-  addDoc(collection(dbService, 'postTest'), data);
+  addDoc(collection(dbService, 'test'), data);
   console.log('데이터가 추가되었습니다.');
 };
 
 //* 스토어에 데이터 삭제하기
 export const deleteData: any = (docId: any) => {
   console.log('docId: ', docId);
-  deleteDoc(doc(dbService, 'postTest', docId));
+  deleteDoc(doc(dbService, 'test', docId));
   console.log('데이터가 삭제되었습니다.');
 };
 
 //* 스토어에 데이터 수정하기
 export const updataData: any = (data: any) => {
   console.log('data: ', data);
-  updateDoc(doc(dbService, 'postTest', data.id), data);
+  updateDoc(doc(dbService, 'test', data.id), data);
   console.log('데이터가 수정되었습니다.');
 };
