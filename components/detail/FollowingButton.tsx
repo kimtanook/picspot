96e40e883953c8e63cdc9b56717b86e92053503c
Kidongg <dlrlehd7@daum.net>
2023-02-17 17:32:59 +0900

    Feat show follwing button to toggle
    Toggle the following button and the delete follow button.
    
    by @kidongg #71

--- components/detail/FollowingButton.tsx
+++ components/detail/FollowingButton.tsx
@@ -1,11 +1,15 @@
 import { addFollowing, deleteFollwing, getFollwing } from '@/api';
 import { authService } from '@/firebase';
-import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { useMutation, useQuery } from 'react-query';
 import styled from 'styled-components';
 
 const FollowingButton = ({ item }: any) => {
-  const router = useRouter();
+  //! FollwingUserAndCreatorUid를 state로 만들어 변경 시 마다 팔로잉, 팔로잉 삭제 버튼 바로 렌더링 되게하기
+  const [follwingUserAndCreatorUidState, setFollwingUserAndCreatorUidState] =
+    useState(false);
+  // console.log('item: ', item);
+  // console.log('data: ', data);
 
   //* mutation 사용해서 팔로잉 데이터 보내기
   const { mutate: follwingMutate } = useMutation(addFollowing, {
@@ -21,6 +25,7 @@ const FollowingButton = ({ item }: any) => {
   const onClickFollowingBtn = (item: any) => {
     console.log('팔로잉 버튼을 클릭했습니다.');
     follwingMutate({ ...item, uid: authService?.currentUser?.uid });
+    setFollwingUserAndCreatorUidState(!follwingUserAndCreatorUidState);
   };
 
   //* mutation 사용해서 팔로잉 삭제 데이터 보내기
@@ -37,49 +42,49 @@ const FollowingButton = ({ item }: any) => {
   const onClickDeleteFollwing = (item: any) => {
     console.log('팔로잉삭제 버튼을 클릭했습니다');
     deleteFollowingMutate({ ...item, uid: authService?.currentUser?.uid });
+    setFollwingUserAndCreatorUidState(!follwingUserAndCreatorUidState);
   };
 
   //* useQuery 사용해서 following 데이터 불러오기
-  const { data, isLoading, isError } = useQuery('followingData', getFollwing);
-
-  //* following 걸러내기
-  // console.log('item: ', item);
-  // console.log('data: ', data);
-  // console.log('authService.currentUser.uid', authService?.currentUser?.uid);
+  const {
+    data: followingData,
+    isLoading,
+    isError,
+  } = useQuery('followingData', getFollwing);
 
   if (isLoading) return <h1>로딩중 입니다</h1>;
   if (isError) return <h1>통신이 불안정합니다</h1>;
 
+  //* 해당 게시물에 대하여 팔로우한 사람은 팔로우 삭제 버튼이 팔로우 하지 않은 사람은 팔로우 추가 버튼을 보이도록 하기
+  //? 팔로잉한 사람 uid를 배열에 담았습니다.
+  const followingUserUid = followingData
+    .filter((item: any) => {
+      return item.uid === authService?.currentUser?.uid;
+    })
+    .find((item: any) => {
+      return item.follow;
+    }).follow;
+  console.log('followingUserUid: ', followingUserUid);
+
+  //? 팔로잉한 사람 uid와 작성자 uid가 같으면 변수에 해당 id가 담깁니다. 없을 경우에는 undefined가 담깁니다.
+  const follwingUserAndCreatorUid = followingUserUid.find((good: any) => {
+    return good === item.creator;
+  });
+  console.log('follwingUserAndCreatorUid: ', follwingUserAndCreatorUid);
+  // setFollwingUserAndCreatorUidState(follwingUserAndCreatorUid);
+
   return (
     <div>
-      {/* 해당 게시물에 대하여 팔로우한 사람은 팔로우 삭제 버튼이 팔로우 하지 않은 사람은 팔로우 추가 버튼을 보이도록 하기!! */}
-      {/* {data
-        .map((following: any) => {
-          console.log('following.id: ', following.id);
-          console.log('following.follow: ', following.follow);
-          return following.follow === id;
-          //following.id === authService?.currentUser?.uid;
-        })
-        .map((follow: any) => (
-          <div key={uuidv4()}>
-            {follow === id ? (
-              <StFollowingBtn onClick={() => onClickFollowingBtn(item)}>
-                팔로잉
-              </StFollowingBtn>
-            ) : (
-              <StFollowingBtn onClick={() => onClickDeleteFollwing(item)}>
-                팔로잉 삭제
-              </StFollowingBtn>
-            )}
-          </div>
-        ))} */}
-
-      <StFollowingBtn onClick={() => onClickFollowingBtn(item)}>
-        팔로잉
-      </StFollowingBtn>
-      <StFollowingBtn onClick={() => onClickDeleteFollwing(item)}>
-        팔로잉 삭제
-      </StFollowingBtn>
+      {follwingUserAndCreatorUidState ? (
+        <StFollowingBtn onClick={() => onClickDeleteFollwing(item)}>
+          팔로잉 삭제
+        </StFollowingBtn>
+      ) : (
+        // 익명함수뒤에 함수 추가가 가능하다
+        <StFollowingBtn onClick={() => onClickFollowingBtn(item)}>
+          팔로잉
+        </StFollowingBtn>
+      )}
     </div>
   );
 };
