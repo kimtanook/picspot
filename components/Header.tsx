16b46dab44804ef5d2ca3a4b866616ebc47dd664
kimtanook <seob6402@gmail.com>
2023-02-21 23:50:54 +0900

    Update main style update main header, form, search style by @kimtanook #106

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,7 +34,9 @@ const Header = () => {
     }
   }, [nowUser]);
   return (
-    <Nav>
+    <HeaderContainer>
+      {/* 로그인, 로그아웃, 마이페이지 버튼 */}
+      {closeLoginModal && <ModalLogin closeModal={closeLoginModalButton} />}
       <Link href="/" style={{ color: 'black', textDecorationLine: 'none' }}>
         <Title
           onClick={() => {
@@ -45,88 +47,71 @@ const Header = () => {
           Picspot
         </Title>
       </Link>
-      {/* 로그인, 로그아웃, 마이페이지 버튼 */}
-      {closeLoginModal && <ModalLogin closeModal={closeLoginModalButton} />}
-      <Profile onClick={() => setIsProfileOpen(!isProfileOpen)}>
-        {authService.currentUser?.photoURL ? (
-          <ProfileImg src={authService.currentUser?.photoURL} />
-        ) : (
-          <ProfileImg src="/profileicon.svg" />
-        )}
-      </Profile>
-      {isProfileOpen === true ? (
-        <Menu>
-          {currentUser ? (
-            <MenuItem onClick={() => router.push('/mypage')}>
-              {' '}
-              마이페이지
-            </MenuItem>
+      <ProfileMenu>
+        <div onClick={() => setIsProfileOpen(!isProfileOpen)}>
+          {authService.currentUser?.photoURL ? (
+            <ProfileImg src={authService.currentUser?.photoURL} />
           ) : (
-            <MenuItem hidden onClick={() => router.push('/mypage')} />
+            <ProfileImg src="/profileicon.svg" />
           )}
-          {currentUser ? (
-            <MenuItem onClick={logOut}>로그아웃</MenuItem>
-          ) : (
-            <MenuItem onClick={closeLoginModalButton}>로그인</MenuItem>
-          )}
-        </Menu>
-      ) : null}
-    </Nav>
+        </div>
+        {isProfileOpen === true ? (
+          <Menu>
+            {currentUser ? (
+              <MenuItem onClick={() => router.push('/mypage')}>
+                {' '}
+                마이페이지
+              </MenuItem>
+            ) : (
+              <MenuItem hidden onClick={() => router.push('/mypage')} />
+            )}
+            {currentUser ? (
+              <MenuItem onClick={logOut}>로그아웃</MenuItem>
+            ) : (
+              <MenuItem onClick={closeLoginModalButton}>로그인</MenuItem>
+            )}
+          </Menu>
+        ) : null}
+      </ProfileMenu>
+    </HeaderContainer>
   );
 };
 export default Header;
 
-const Nav = styled.div`
-  /* position: fixed; */
+const HeaderContainer = styled.div`
+  width: 1440px;
+  position: fixed;
   display: flex;
   justify-content: space-between;
   height: 70px;
-  width: 80px;
+  z-index: 10;
 `;
-const Title = styled.h1`
-  box-shadow: inset 0 -3px 0 0 red;
-  box-shadow: none;
-  font-size: 40px;
+const Title = styled.div`
   font-weight: 900;
-  margin-left: 30px;
+  font-size: 24px;
   cursor: pointer;
 `;
-const Profile = styled.div`
-  position: absolute;
-  top: 15px;
-  right: 20px;
-  z-index: 999;
-  width: 70px;
-  height: 70px;
-  border-radius: 50px;
-  background-color: white;
-  cursor: pointer;
-`;
-const ProfileImg = styled.img`
-  margin-left: auto;
-  width: 70px;
-  height: 70px;
-  border-radius: 50px;
-  object-fit: cover;
-  position: fixed;
-  top: 10;
-  left: 80;
-  right: 20;
-`;
+const ProfileMenu = styled.div``;
 const Menu = styled.div`
   width: 100px;
   height: 85px;
   position: absolute;
-  top: 95px;
-  right: 20px;
+  right: 10px;
   background-color: orange;
   border-radius: 5px;
-  box-shadow: 1px 1px 1px orange;
   text-align: center;
   color: white;
-  font-family: GmarketSans;
   z-index: 6000;
 `;
 const MenuItem = styled.p`
+  cursor: pointer;
+  :hover {
+    color: black;
+  }
+`;
+const ProfileImg = styled.img`
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
   cursor: pointer;
 `;
