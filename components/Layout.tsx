1b56e46b2b4cec20dcdcebc444bdb7f70236ac53
kimtanook <seob6402@gmail.com>
2023-02-28 00:09:54 +0900

    Feat  message service add message modal the layout component by @kimtanook #161

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,39 @@
+import { messageBoxToggle, messageSendToggle } from '@/atom';
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import { CustomModal } from './common/CustomModal';
+import MessageBox from './message/MessageBox';
+import MessageSend from './message/MessageSend';
 
 function Layout() {
-  return <div>Layout</div>;
+  const [msgBoxToggle, setMsgBoxToggle] = useRecoilState(messageBoxToggle);
+  const [msgSendToggle, setMsgSendToggle] = useRecoilState(messageSendToggle);
+  return (
+    <div>
+      <div>
+        {msgBoxToggle ? (
+          <CustomModal
+            modal={msgBoxToggle}
+            setModal={setMsgBoxToggle}
+            width={'500'}
+            height={'500'}
+            element={<MessageBox />}
+          />
+        ) : null}
+      </div>
+      <div>
+        {msgSendToggle ? (
+          <CustomModal
+            modal={msgSendToggle}
+            setModal={setMsgSendToggle}
+            width={'500'}
+            height={'500'}
+            element={<MessageSend />}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
 export default Layout;
