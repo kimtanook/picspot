d89063c2e001fc03e5bebd22edf4ffe2fe8b355a
kimtanook <seob6402@gmail.com>
2023-03-02 12:20:07 +0900

    Feat landing page mobile version add mobile version on the landing page by @kimtanook #191

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Layout from '@/components/Layout';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
 
 const client = new QueryClient({
   defaultOptions: {
@@ -11,13 +12,18 @@ const client = new QueryClient({
     },
   },
 });
+const theme = {
+  mobile: `(max-width: 425px)`,
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={client}>
       <RecoilRoot>
-        <Layout />
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Layout />
+          <Component {...pageProps} />
+        </ThemeProvider>
       </RecoilRoot>
     </QueryClientProvider>
   );
