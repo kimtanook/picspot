ca7ee33deec0bcb07f00e04e3daad1659ca17cb2
hotsix <codinggo@naver.com>
2023-03-08 15:10:45 +0900

    Add auth sign up mobile version add auth sign up mobile version by @waterfist #214

--- firebase.tsx
+++ firebase.tsx
@@ -4,12 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC0ZFhe2fsGDxsiXg8zCZIkwr3d3vGpo20',
-  authDomain: 'picspot22-a7280.firebaseapp.com',
-  projectId: 'picspot22-a7280',
-  storageBucket: 'picspot22-a7280.appspot.com',
-  messagingSenderId: '1015895283247',
-  appId: '1:1015895283247:web:f960a7398c0b46de5798da',
+  apiKey: 'AIzaSyDXkgOAqRO1Wo-QjCEqn1LVsyGTkzTkKq0',
+  authDomain: 'picspotyujin.firebaseapp.com',
+  projectId: 'picspotyujin',
+  storageBucket: 'picspotyujin.appspot.com',
+  messagingSenderId: '22909805217',
+  appId: '1:22909805217:web:1f923ba25f3343d32b46e0',
+  measurementId: 'G-C0GKQ3RZ4H',
 };
 
 const app = initializeApp(firebaseConfig);
