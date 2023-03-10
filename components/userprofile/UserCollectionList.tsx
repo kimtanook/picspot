25d5713fc69f1b64687ea58ce6708ea20e00194e
kimtanook <seob6402@gmail.com>
2023-03-10 14:39:18 +0900

    Update user profile edit get collection data code, add stale time

--- components/userprofile/UserCollectionList.tsx
+++ components/userprofile/UserCollectionList.tsx
@@ -1,4 +1,4 @@
-import { getCollection } from '@/api';
+import { getCollection, getUserCollection } from '@/api';
 import { useQuery } from 'react-query';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 import styled from 'styled-components';
@@ -6,19 +6,18 @@ import { v4 as uuidv4 } from 'uuid';
 import UserCollectionTown from './UserCollectionTown';
 
 const UserCollectionList = ({ userId }: { userId: string }) => {
-  //* useQuery 사용해서 collection 데이터 불러오기
+  //* 특정 유저의 collection 데이터 불러오기
   const { data: collectionData, isLoading } = useQuery(
-    'UserCollection',
-    getCollection
+    ['UserCollection', userId],
+    getUserCollection,
+    {
+      staleTime: 1000 * 60 * 5,
+      cacheTime: 1000 * 60 * 10,
+    }
   );
-  console.log('collectionData : ', collectionData);
-  // filter로 거른다 (collectionData의 collector 중에서, userId가 true인 item을. )
-  const userCollectPost = collectionData?.filter((item: userItem) => {
-    return item.collector?.find((item: string) => userId.includes(item));
-  });
 
   // town값만 추출
-  const townArray = userCollectPost?.map((item: userItem) => {
+  const townArray = collectionData?.map((item: userItem) => {
     return item.town;
   });
 
@@ -30,17 +29,21 @@ const UserCollectionList = ({ userId }: { userId: string }) => {
   );
   return (
     <GridBox>
-      <ResponsiveMasonry columnsCountBreakPoints={{ 425: 1, 750: 2, 1200: 3 }}>
-        <Masonry columnsCount={3}>
-          {uniqueTownArray?.map((item: string) => (
-            <UserCollectionTown
-              key={uuidv4()}
-              value={item}
-              postList={userCollectPost}
-            />
-          ))}
-        </Masonry>
-      </ResponsiveMasonry>
+      {typeof window === 'undefined' ? null : (
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{ 425: 1, 750: 2, 1200: 3 }}
+        >
+          <Masonry columnsCount={3}>
+            {uniqueTownArray?.map((item: string) => (
+              <UserCollectionTown
+                key={uuidv4()}
+                value={item}
+                postList={collectionData}
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      )}
     </GridBox>
   );
 };
