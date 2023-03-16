4a432ad50b0a75e1525fe082dd0b56bcea9fc550
Jaeyoung <dlawodud96@naver.com>
2023-03-16 17:01:05 +0900

    Fix mypage empty image changed mypage empty image css by @Imjaae #327

--- components/mypage/CollectionList.tsx
+++ components/mypage/CollectionList.tsx
@@ -51,7 +51,7 @@ const CollectionList = () => {
             가보고 싶은 여행지 사진을 찾으러 가볼까요?
           </EmptyContetnts>
           <EmptyBtn onClick={() => router.push('/main?city=제주전체')}>
-            제주도 전체 사진 둘러보기
+            제주도 전체 사진 둘러보기 {'>'}
           </EmptyBtn>
         </EmptyPostBox>
       ) : (
@@ -92,8 +92,8 @@ const EmptyPostBox = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
   & > .empty-image {
-    width: 100%;
-    height: 100%;
+    width: 200px;
+    height: 173px;
   }
 `;
 
@@ -106,8 +106,9 @@ const EmptyTitle = styled.div`
 const EmptyContetnts = styled.div`
   font-size: 24px;
   font-weight: 700;
-  width: 260px;
+  width: 250px;
   text-align: center;
+  margin-bottom: 10px;
 `;
 const EmptyBtn = styled.button`
   color: #1882ff;
